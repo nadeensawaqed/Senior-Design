570502b604024f793885e1eb63cf401f2eb74e27
Haythem Mansour <haythem.mansour@ucdenver.edu>
2021-11-24 12:40:36 -0700

    Wrote stuff

--- AprilTag_Detection/aprilTag.py
+++ AprilTag_Detection/aprilTag.py
@@ -1,1 +1,16 @@
 # This will detect stuff
+import cv2
+
+vid = cv2.VideoCapture(0)
+
+while(true):
+    ret, frame = vid.read()
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+vid.release()
+
+cv2.destroyAllWindows()
